0cec59e040de6daea837490689d1fc33c7429be6
LK Loh <lkloh2410@gmail.com>
2017-07-24 09:57:18 -0700

    Added database example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # django_db_optimization
 Examples of Django db optimization in practice.
+
+
+Demo of [select_relatedß](https://timmyomahony.com/blog/misconceptions-select_related-in-django/) in practice. 
--- select_cache/models.py
+++ select_cache/models.py
@@ -4,3 +4,13 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+	name = models.CharField(max_length=200)
+
+class Album(models.Model):
+	name = models.CharField(max_length=200)
+	genre = models.ForeignKey(Genre)
+
+
+
